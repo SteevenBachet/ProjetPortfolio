90bde1d5ed3ad25778c9a6d8702325f9cd7954a0
Steeven Bachet <steevy72500@gmail.com>
2024-05-30 16:10:48 +0200

    contact terminé + skills terminé

--- src/parts/skills/index.jsx
+++ src/parts/skills/index.jsx
@@ -6,13 +6,34 @@ function Skills() {
             <h2 className='skills__title'>COMPETENCES</h2>
             <div className='skills__container'>
                 <div className='skills__container__item'>
-                    Skill
+                    <i class="fa-brands fa-html5"></i>
+                    <p className='skills__container__item__title'>Front-end</p>
+                    <ul className='skills__container__item__list'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>React</li>
+                    </ul>
                 </div>
                 <div className='skills__container__item'>
-                    Skill
+                    <i class="fa-solid fa-database"></i>
+                    <p className='skills__container__item__title'>Back-end</p>
+                    <ul className='skills__container__item__list'> 
+                        <li>Node.js</li>
+                        <li>MongoDB</li>
+                        <li>Express.js</li>
+                        <li>Création et utilisation API</li>    
+                    </ul>
                 </div>
                 <div className='skills__container__item'>
-                    Skill
+                    <i class="fa-solid fa-wrench"></i>
+                    <p className='skills__container__item__title'>Outils</p>
+                    <ul className='skills__container__item__list'>
+                        <li>ChatGPT</li>
+                        <li>npm</li>
+                        <li>Github</li>
+                        <li>Postman</li>
+                    </ul>
                 </div>
             </div>
         </div>
